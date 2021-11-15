9cc939c8d4a4327a205e76de606768c355e79dd2
Suracheth Chawla <dev1987@gmail.com>
2021-11-15 12:12:59 +0700

    init project

---
+++ lib/oban_telemetry.ex
@@ -0,0 +1,18 @@
+defmodule ObanTelemetry do
+  @moduledoc """
+  Documentation for `ObanTelemetry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ObanTelemetry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oban_telemetry_test.exs
@@ -0,0 +1,8 @@
+defmodule ObanTelemetryTest do
+  use ExUnit.Case
+  doctest ObanTelemetry
+
+  test "greets the world" do
+    assert ObanTelemetry.hello() == :world
+  end
+end
